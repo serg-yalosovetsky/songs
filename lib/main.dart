21ad204345e8a5613eb41bf887fab4ef191ac2cd
syalosovetskyi <syalosovetskyi@zeushq.com>
2024-09-12 05:49:42 +0300

    license

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Flutter Song Lyrics App.
+ *
+ * Flutter Song Lyrics App is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Flutter Song Lyrics App is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Flutter Song Lyrics App.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
